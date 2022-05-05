0cab9339fd6951f291af8ffa0812b841366e48af
Nyk Stobbe <nykstobbe@hotmail.com>
2022-05-05 20:34:35 +0200

    voorbeeld terug en kleine changes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
+import WebSocket from "ws";
+
 import { config } from 'dotenv';
 import SessionData from './state/sessionData';
 import tick from './state/tick';
 import websocket from './websocket/initialize';
-import WebSocket from "ws";
 
 if(config({ path: './.env' }).error != undefined) {
     console.log("error finding .env file")
 };
 
 let sessiondata: SessionData = new SessionData();
-let tickRate: number = parseInt(process.env.TICKRATE!);
 
-let ws: WebSocket = websocket(sessiondata);
+let tickRate: number = parseInt(process.env.TICKRATE!);
+let url: string = process.env.URL!;
+
+let ws: WebSocket = websocket(url, sessiondata);
 setInterval(tick, tickRate, ws, sessiondata, tickRate);
--- src/websocket/initialize.ts
+++ src/websocket/initialize.ts
@@ -3,8 +3,8 @@ import onMessage from "./onMessage";
 import onOpen from "./onOpen";
 import SessionData from "../state/sessionData";
 
-function initialize(sessionData: SessionData) {
-    const ws = new WebSocket(process.env.URL!);
+function initialize(url: string, sessionData: SessionData) {
+    const ws = new WebSocket(url);
     
     ws.on('open', () => { onOpen(ws, sessionData) });
     ws.on('message', (data) => { onMessage(ws, sessionData, data) });
