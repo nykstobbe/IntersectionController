a4d83fecccc1816ca05674901cb96df900dd1cc1
Nyk Stobbe <nykstobbe@hotmail.com>
2022-05-09 21:57:26 +0200

    bridge work ahead

--- src/state/sessionData.ts
+++ src/state/sessionData.ts
@@ -1,10 +1,21 @@
 export default class SessionData {
     private entityInRouteCount: Map<number, number>;
+    private bridgeQueue: Array<number>
     private running: boolean;
+    private handlingBridge: boolean;
 
     public constructor() {
         this.running = false;
         this.entityInRouteCount = new Map();
+        this.handlingBridge = false;
+        this.bridgeQueue = new Array();
+    }
+
+    public clear() {
+        this.running = false;
+        this.entityInRouteCount = new Map();
+        this.handlingBridge = false;
+        this.bridgeQueue = new Array();
     }
 
     public setRunning(running: boolean) {
@@ -22,6 +33,10 @@ export default class SessionData {
         
         const count = this.entityInRouteCount.get(routeId)!;
         this.entityInRouteCount.set(routeId, count + 1);
+
+        if(routeId > 40) {
+            this.bridgeQueue.push(routeId);
+        }
     }
 
     public decrementRouteCount(routeId: number) {
@@ -31,6 +46,14 @@ export default class SessionData {
             this.entityInRouteCount.set(routeId, count - 1);
         else
             this.entityInRouteCount.delete(routeId);
+
+        if(routeId > 40) {
+            for (let i = 0; i < this.bridgeQueue.length; i++) {
+                if (this.bridgeQueue[i] == routeId) {
+                    this.bridgeQueue.splice(i);
+                }
+            }
+        }
     }
 
     public getRouteCount(routeId: number): number {
@@ -43,5 +66,25 @@ export default class SessionData {
     
     public getRoutesWithEntitites(): number[] {
         return Array.from(this.entityInRouteCount.keys());
+    }
+
+    public isHandlingBridge() {
+        return this.handlingBridge;
+    }
+
+    public setHandlingBridge(handlingBridge: boolean) {
+        this.handlingBridge = handlingBridge;
+    }
+
+    public pushBridgeQueue(routeId: number) {
+        this.bridgeQueue.push(routeId);
+    }
+
+    public popFromBridgeQueue() {
+        return this.bridgeQueue.shift();
+    }
+
+    public getFromBridgeQueue(index: number) {
+        return this.bridgeQueue[index];
     }
 }
